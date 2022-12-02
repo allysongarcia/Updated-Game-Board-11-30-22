357ea440470c0785c2c3fabe979052fc97cb1df7
allysongarcia <98438254+allysongarcia@users.noreply.github.com>
2022-12-01 18:18:47 -0600

    12 1 2022

--- Assets/diceRollScript.cs
+++ Assets/diceRollScript.cs
@@ -6,12 +6,22 @@ using UnityEngine.UI;
 public class diceRollScript : MonoBehaviour
 {
     public Text number;
+    public Text total;
     public int DiceValue;
+    public int DiceValue2;
+    public int DiceTotal;
 
     public void diceRoll( ) 
     {       
            DiceValue = Random.Range(1, 7);
-           number.text = "You rolled " + DiceValue + "!";
+           DiceValue2 = Random.Range(1,7);
+           DiceTotal = DiceValue + DiceValue2;
+           number.text = "You rolled " + DiceValue + " and " + DiceValue2 + "!\nMove " + DiceTotal + " spaces!";
            //Debug.Log("Rolled: " + DiceValue);
+    }
+
+    public void getTotal( )
+    {
+    	total.text = "Spaces to Move: " + DiceTotal;
     }
 }
